ddfe2631743d9290c033b149ab5c7f2afae68450
ishaniff <167576494+ishaniff@users.noreply.github.com>
2024-11-02 19:16:14 -0400

    scrapping radio buttons, reverting to buttons

--- script.js
+++ script.js
@@ -5,8 +5,19 @@ const submission = {
 };
 let playerAction;
 let logic;
-let computerAction = Math.floor(Math.random() * 3);
-let computerLogic = Math.floor(Math.random() * 3);
+let computerChoice;
+// let computerAction = Math.floor(Math.random() * 3);
+
+function computerAction() {
+  computerChoice = Math.random();
+  if (computerChoice > 0.66) {
+    console.log("rock");
+  } else if (computerChoice > 0.33) {
+    console.log("paper");
+  } else {
+    console.log("scissors");
+  }
+}
 function btnRock() {
   playerAction = submission.action[0];
   logic = submission.logic[0];
@@ -28,15 +39,9 @@ function btnScissor() {
 function game() {
   if (playerAction == submission.action[computerAction]) {
     console.log(submission.decision[2]);
-  } else if (logic < submission.logic[computerLogic]) {
+  } else if (playerAction) {
     console.log(submission.decision[1]);
   } else {
     console.log(submission.decision[0]);
   }
 }
-
-// } else if (logic < submission.logic[computerLogic]) {
-//   console.log(submission.decision[0]);
-// }
-
-document.getElementById("wins").innerText = submission.action;
