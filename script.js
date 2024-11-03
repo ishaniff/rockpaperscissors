60e457c2bcc8494d92faf0d633267405e9f403ec
ishaniff <167576494+ishaniff@users.noreply.github.com>
2024-11-03 15:39:05 -0500

    updated comments

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let playerAction;
-// let logic;
+// let logic; was going to use this to do declare whos winner or loser.. made no sense. wasted lot of time
 let computerChoice;
 let wins = 0;
 let losses = 0;
 let ties = 0;
+
+// prompts the player upon loading of site to input a name and then stores it and calls it later to dynamically use name in my page
 let playerName = prompt(`What's your name?`);
 document.getElementById("playerName").innerText = `${playerName}'s Actions`;
 
+// this is my object that contains my actions, decisions, and tracker
 const rpsObject = {
   action: ["rock", "paper", "scissors"],
   // logic: [1, 2, 3], was going to use this to do declare whos winner or loser.. made no sense. wasted lot of time
@@ -15,6 +18,7 @@ const rpsObject = {
 };
 // let computerAction = Math.floor(Math.random() * 3); at first I had it like this and then I realized I could do it another way too.
 
+// this will generate a choice for the computer being rock paper or scissors, it also will set the corresponding choice as an image on the page
 function computerAction() {
   computerChoice = Math.random();
   if (computerChoice > 0.66) {
@@ -34,6 +38,8 @@ function computerAction() {
     ).innerHTML = `<img src="/images/SCISSORS.jpg">`;
   }
 }
+
+// this will set the player choice to rock and set the picture to correspond with the choice as well, it also runs the rng computer choice function above and the game function below
 function btnRock() {
   playerAction = rpsObject.action[0];
   document.getElementById(
@@ -62,6 +68,7 @@ function btnScissor() {
   game();
 }
 
+// this right here is the core functionality of the game, its comparing option choices between player and computer, outputting the outcomes in both text and images, adding a tracker value for wins losses and ties, displays tracker values in a table as well
 function game() {
   if (playerAction == computerChoice) {
     document.getElementById(
@@ -97,6 +104,7 @@ function game() {
   document.getElementById("ties").innerText = `${rpsObject.tracker[2]}`;
 }
 
+// makes the reset button the scoreboard reset the values to zero for win loss tie and as well as visually clears eventlogs and updates text to be blank
 function trackerReset() {
   rpsObject.tracker[0] = 0;
   rpsObject.tracker[1] = 0;
@@ -109,16 +117,3 @@ function trackerReset() {
   document.getElementById("eventOutcome").innerHTML = ``;
   document.getElementById("eventComputer").innerHTML = ``;
 }
-// document.getElementById(
-//   "eventPlayer"
-// ).innerHTML = `<img src="/images/LOST.png">`;
-// document.getElementById(
-//   "eventOutcome"
-// ).innerHTML = `<img src="/images/LOST.png">`;
-// document.getElementById(
-//   "eventComputer"
-// ).innerHTML = `<img src="/images/LOST.png">`;
-
-// function actionDisplayer() {
-//   if (playerAction == "rock") console.log("working");
-// }
